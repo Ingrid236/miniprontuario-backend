4b72e73e71e7087caf855c4fab928eac8e6823af
Ingrid <ingrid.236@aluno.ueg.br>
2026-05-29 20:29:16 -0300

    Initial commit from Specify template

---
+++ miniprontuario-backend/src/main/java/com/miniprontuario/miniprontuario_backend/MiniprontuarioBackendApplication.java
@@ -0,0 +1,13 @@
+package com.miniprontuario.miniprontuario_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniprontuarioBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniprontuarioBackendApplication.class, args);
+	}
+
+}
---
+++ miniprontuario-backend/src/test/java/com/miniprontuario/miniprontuario_backend/MiniprontuarioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miniprontuario.miniprontuario_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniprontuarioBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
